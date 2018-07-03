44c08cb101643f870639f939cf1dfdaf3026ac05
EdddieN <39434245+EdddieN@users.noreply.github.com>
2018-07-03 16:11:37 +0100

    Create reset-machinon.sh

---
+++ files/scripts/reset-machinon.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Machinon support script for MCU software reset via serial command or hardware reset via Raspberry Pi GPIO pulse
+# www.machinon.com
+#
+# Usage:
+#   reset-machinon.sh -m      Reset the Machinon main microcontroller and trigger its bootloader
+#   reset-machinon.sh -s      Reset the Machinon slave microcontroller and trigger its bootloader
+#
+
+if [[ "$1" = "-s" ]] ; then
+    # send command to reset slave
+    echo "Resetting Machinon Slave AVR..."
+
+    # config serial port for 115200 baud, 8N1, no handshaking (default for Machinon config serial port)
+    serialport=/dev/ttySC1
+    stty -F $serialport raw ispeed 115200 ospeed 115200 cs8 -crtscts
+    exec <> $serialport  # hold serial port open for duration of script
+    IFS=$'\n'       # make newlines the only separator in files (default for MySensors messages as used on MiniBMS/Machinon2)
+    # send a couple of LF to flush/clear the Machinon parser
+    echo -e "\n" > $serialport
+
+    # send MySensors request to Main AVR for it to reset Slave AVR
+    echo "0;1;1;1;25;2" > $serialport
+    # expect an ACK reply with same message content
+    read -t 0.5 -s response < $serialport || { echo "Timeout getting response"; exit 1; }
+    if [[ "$response" = "0;1;1;1;25;2" ]] ; then
+        echo "Slave reset OK"
+        exit 0
+    else
+        echo "Slave reset failed!"
+        exit 1
+    fi
+else
+    # do direct hardware reset of Main AVR
+    echo "Resetting Machinon Main AVR..."
+
+    # Enable GPIO 23 and set to output
+    echo "23" > /sys/class/gpio/export
+    sleep 0.5  # allow time for filesystem changes
+    echo "out" > /sys/class/gpio/gpio23/direction
+     
+    # Write a high pulse to GPIO23
+    echo "1" > /sys/class/gpio/gpio23/value
+    sleep 0.05
+    echo "0" > /sys/class/gpio/gpio23/value
+
+    # Clean up
+    echo "23" > /sys/class/gpio/unexport
+fi
+#end

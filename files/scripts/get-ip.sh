bb7b002fad55671e5383e27b17d88eb4fa09218c
MrVolkerball <43345503+jjgarciajz@users.noreply.github.com>
2019-01-21 13:38:27 +0100

    Update get-ip.sh
    Changed tabs to spaces and unix return carriages for better copy/pasting.

--- files/scripts/get-ip.sh
+++ files/scripts/get-ip.sh
@@ -13,21 +13,21 @@ ip_type=" D"
 
 # if that didn't work, get the current IP of the eth0 interface
 if [ -z $my_ip ] ; then
-	# got empty result from last test
-	#echo "No IP for default route"
-	my_ip=$(hostname -I)
-	# another option from https://unix.stackexchange.com/questions/8518/how-to-get-my-own-ip-address-and-save-it-to-a-variable-in-a-shell-script
-	#my_ip=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-	# or another from https://unix.stackexchange.com/questions/8518/how-to-get-my-own-ip-address-and-save-it-to-a-variable-in-a-shell-script
-	#my_ip=$(ip -o addr show up primary scope global | while read -r num dev fam addr rest; do echo ${addr%/*}; done)
+    # got empty result from last test
+    #echo "No IP for default route"
+    my_ip=$(hostname -I)
+    # another option from https://unix.stackexchange.com/questions/8518/how-to-get-my-own-ip-address-and-save-it-to-a-variable-in-a-shell-script
+    #my_ip=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+    # or another from https://unix.stackexchange.com/questions/8518/how-to-get-my-own-ip-address-and-save-it-to-a-variable-in-a-shell-script
+    #my_ip=$(ip -o addr show up primary scope global | while read -r num dev fam addr rest; do echo ${addr%/*}; done)
   
-	ip_type=" S"
-	if [ -z $my_ip ] ; then
-		# got empty result from last test
-		#echo "No IP for default adapter"
-		my_ip="unknown"
-		ip_type=""
-	fi
+    ip_type=" S"
+    if [ -z $my_ip ] ; then
+        # got empty result from last test
+        #echo "No IP for default adapter"
+        my_ip="unknown"
+        ip_type=""
+    fi
 fi
 
 echo ${my_ip}
